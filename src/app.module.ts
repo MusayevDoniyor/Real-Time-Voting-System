4583fe61b20ca827f9e998b2b125776f291c64cf
MusayevDoniyor <doniyormusayev2009@gmail.com>
2025-05-23 17:36:44 +0500

    add typeorm postgres connection, environment variables configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env', 'example.env'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => {
+        console.log("🔄 Ma'lumotlar bazasiga ulanish jarayoni boshlandi...");
+
+        const dbConfig: TypeOrmModuleOptions = {
+          type: 'postgres',
+          host: configService.get<string>('DATABASE_HOST'),
+          port: configService.get<number>('DATABASE_PORT'),
+          username: configService.get<string>('DATABASE_USER'),
+          password: configService.get<string>('DATABASE_PASSWORD'),
+          database: configService.get<string>('DATABASE_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+
+        try {
+          console.log(
+            "✅ Ma'lumotlar bazasi konfiguratsiyasi yuklandi:",
+            dbConfig,
+          );
+          console.log('🚀 Ulash muvaffaqiyatli yakunlandi!');
+          return dbConfig;
+        } catch (error) {
+          console.error('❌ Ulashda xatolik yuz berdi:', error);
+          throw error;
+        }
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
